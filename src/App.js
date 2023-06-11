8c5b4a9dc13ee7df2e12ec1954e65b65f458c944
Shantanu <gautam.hice@gmail.com>
2023-06-11 15:02:34 +0530

    Setting Axiom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Component/Header";
 import Footer from "./Component/Footer";
@@ -11,71 +10,90 @@ import Missing from "./Component/Missing";
 import { useState, useEffect } from "react";
 import { useHistory, Route, Switch } from "react-router-dom";
 import { format } from "date-fns";
+import api from "./api/posts";
+import EditPost from './Component/EditPost'
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "First Post",
-      datetime: "July 01,2021 11:18:25 AM",
-      body: "kjbvskjladiovbavnkjabdnvkadsbvks v,basv sdkb vkdsn v,bsdBVK ,mBVkjd v mn,dskhvB S< DVkhsdb mnsd khj",
-    },
-    {
-      id: 2,
-      title: "Second Post",
-      datetime: "July 01,2021 11:18:25 AM",
-      body: "kjbvskjladiovbavnkjabdnvkadsbvks v,basv sdkb vkdsn v,bsdBVK ,mBVkjd v mn,dskhvB S< DVkhsdb mnsd khj",
-    },
-    {
-      id: 3,
-      title: "Third Post",
-      datetime: "July 01,2021 11:18:25 AM",
-      body: "kjbvskjladiovbavnkjabdnvkadsbvks v,basv sdkb vkdsn v,bsdBVK ,mBVkjd v mn,dskhvB S< DVkhsdb mnsd khj",
-    },
-    {
-      id: 4,
-      title: "Fourth Post",
-      datetime: "July 01,2021 11:18:25 AM",
-      body: "kjbvskjladiovbavnkjabdnvkadsbvks v,basv sdkb vkdsn v,bsdBVK ,mBVkjd v mn,dskhvB S< DVkhsdb mnsd khj",
-    },
-    {
-      id: 5,
-      title: "Fifth Post",
-      datetime: "July 01,2021 11:18:25 AM",
-      body: "kjbvskjladiovbavnkjabdnvkadsbvks v,basv sdkb vkdsn v,bsdBVK ,mBVkjd v mn,dskhvB S< DVkhsdb mnsd khj",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchReasults] = useState([]);
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState("");
   const history = useHistory();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const id = posts.length ? posts[posts.length -1].id  + 1 : 1;
-    const datetime = format(new Date(), "MMMM dd,yyyy pp");
-    const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [ ...posts, newPost ];
-    setPosts(allPosts);
-    setPostTitle("");
-    setPostBody("");
-    history.push("/");
-  };
-
-  const handleDelete = (id) => {
-    const postsList = posts.filter((post) => post.id !== id);
-    setPosts(postsList);
-    history.push("/");
-  };
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
 
   useEffect(() => {
-    const filteredResults = posts.filter(post =>
-      ((post.body).toLowerCase()).includes(search.toLowerCase())
-      || ((post.title).toLowerCase()).includes(search.toLowerCase())
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get("/posts");
+        setPosts(response.data);
+      } catch (err) {
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        }
+        else{
+          console.log(`Error:${err.message}`)
+        }
+      }
+    }
+    fetchPosts();
+  }, []);
+
+  
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
     );
-    setSearchReasults(filteredResults.reverse() )
+    setSearchReasults(filteredResults.reverse());
   }, [posts, search]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd,yyyy pp");
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    try{
+      const response= await api.post('/posts',newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      history.push('/');
+    }catch(err){
+      console.log(`Error:${err.message}`);
+    } 
+  }
+
+  const handleEdit=async(id)=>{
+    const datetime=format(new Date(),'MMMM dd,yyyy pp');
+    const updatedPost={id,title:editTitle,datetime,body:editBody};
+    try{
+      const response=await api.put(`/post/${id}`,updatedPost)
+      setPosts(posts.map(post=>post.id===id ? {...response.data} : post));
+      setEditTitle('');
+      setEditBody('');
+      history.push('/');
+    }catch(err){
+      console.log(`Error: ${err.message}`);
+    }
+  }
+
+  const handleDelete =  async(id) => {
+    try{
+      await api.delete(`/posts/${id}`);
+      const postsList = posts.filter((post) => post.id !== id);
+      setPosts(postsList);
+      history.push("/");
+    }catch(err){
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
 
   return (
     <div className="App">
@@ -92,6 +110,16 @@ function App() {
             setPostTitle={setPostTitle}
             postBody={postBody}
             setPostBody={setPostBody}
+          />
+        </Route>
+        <Route path="/edit/:id">
+          <EditPost
+            posts={posts}
+            handleEdit={handleEdit}
+            editTitle={editTitle}
+            setEditTitle={setEditTitle}
+            editBody={editBody}
+            setEditBody={setEditBody}
           />
         </Route>
         <Route path="/post/:id">
